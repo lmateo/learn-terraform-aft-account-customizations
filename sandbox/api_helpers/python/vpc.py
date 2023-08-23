4095de8f682e985e494cc08d2eccb4f0649cd5a5
Lorenzo <Renzo_3011@hotmail.com>
2023-08-23 15:33:32 -0400

    Updated Create and Delete Custom VPC script

--- sandbox/api_helpers/python/vpc.py
+++ sandbox/api_helpers/python/vpc.py
@@ -7,6 +7,11 @@ def client_init():
 def vpc_creation(client):
     client.create_vpc(CidrBlock='172.16.0.0/16')
     
+def vpc_deletion(client, vpc_id):
+    client.delete_vpc(VpcId=vpc_id)
+    
 if __name__ == '__main__':
     client = client_init()
-    vpc_creation(client)
+    vpc_id = 'vpc-09b45fb755f07d73f'
+    vpc_deletion(client, vpc_id)
+    
