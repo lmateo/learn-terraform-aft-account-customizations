a01a6bec7386ba147b49737044529e609b7b3865
Lorenzo <Renzo_3011@hotmail.com>
2023-08-23 12:58:21 -0400

    Updated s3 and ssm file

--- sandbox/terraform/ssm.tf
+++ sandbox/terraform/ssm.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameters" "ssm_sandbox_parameters"{
+resource "aws_ssm_parameter" "ssm_sandbox_parameters"{
     name    = "/accID"
     type    = "String"
     value   = "${data.aws_caller_identity.current.account_id}"
