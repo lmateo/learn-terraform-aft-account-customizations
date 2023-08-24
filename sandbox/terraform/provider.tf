6642c33f6f31038ea4152c0b0e0fe4a572d8d3e5
Lorenzo <Renzo_3011@hotmail.com>
2023-08-24 12:38:38 -0400

    Updated Provider jija file

--- sandbox/terraform/provider.tf
+++ sandbox/terraform/provider.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.22"
-    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = ">= 3.4.0"
