64ede871349958dc2f3ab156d057e61839afd6ad
Lorenzo <Renzo_3011@hotmail.com>
2023-08-24 12:29:44 -0400

    Updated Provider file to tf

---
+++ sandbox/terraform/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.22"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 3.4.0"
+    }
+  }
+}
+
+
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
