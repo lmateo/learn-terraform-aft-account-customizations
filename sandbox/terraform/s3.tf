6b8888d42f8e6b26d90e8964cd8c0dc7eeb956f4
Lorenzo <Renzo_3011@hotmail.com>
2023-09-22 17:32:20 -0400

    Create fcf Wordpress Infrasture

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -3,3 +3,25 @@ resource "aws_s3_bucket" "sandbox_bucket" {
 }
 
 # Use the `aws_s3_bucket_acl` resource to set the ACL for the bucket
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = "${aws_s3_bucket.sandbox_bucket.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::127311923021:root"
+      },
+      "Action": [ "s3:PutObject" ],
+      "Resource": [
+        "${aws_s3_bucket.sandbox_bucket.arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
---
+++ sandbox/terraform/variables.tf
@@ -0,0 +1,66 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "renzo_devops"
+}
+
+variable "inbound_port_production_ec2" {
+  type        = list(any)
+  default     = [22, 80]
+  description = "inbound port allow on production instance"
+}
+
+variable "db_name" {
+  type    = string
+  default = "wordpressdb"
+}
+
+variable "db_user" {
+  type    = string
+  default = "admin"
+}
+
+variable "db_password" {
+  type    = string
+  default = "Wordpress-AWS2Tier"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "key_name" {
+  type    = string
+  default = "wordpress-dev-key"
+}
+
+variable "availability_zone" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidrs" {
+  type        = list(string)
+  description = "list of all cidr for subnet"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+}
+
+variable "target_application_port" {
+  type    = string
+  default = "80"
+}
